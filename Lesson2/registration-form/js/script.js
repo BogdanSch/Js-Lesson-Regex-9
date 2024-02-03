402bbde5d8b819747bb9ae3f1c618d5ca2c047f6
Bohdan <bogmira777@gmail.com>
2024-02-03 22:39:32 +0100

    Add registration form task

--- Lesson2/registration-form/js/script.js
+++ Lesson2/registration-form/js/script.js
@@ -1,24 +1,44 @@
-const orderForm = document.querySelector("#orderForm");
+const registrationForm = document.forms["orderForm"];
+registrationForm.addEventListener("submit", validateForm);
 
-orderForm.addEventListener("submit", verifyForm);
+const loginInput = registrationForm.elements["login"];
+const emailInput = registrationForm.elements["email"];
+const phoneInput = registrationForm.elements["phone"];
+const passwordInput = registrationForm.elements["password"];
+const formErrorMessage = document.getElementById("formErrorMessage");
 
-function verifyForm() {
-  const lastNameField = orderForm.lname;
-  const phoneField = orderForm.phone;
+const loginRegex = /^[a-zA-Z][a-zA-Z0-9_]{7,19}$/;
+const passwordRegex = /^(?=.*[A-Z])[a-zA-Z][a-zA-Z0-9_*]{7,19}$/;
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const phoneRegex = /^[0-9]{10}$/;
 
-  if (lastNameField.value.length == 0) {
-    alert("Уведіть прізвище");
-    lastNameField.focus();
+function showErrorMessage(message) {
+  formErrorMessage.classList.remove("hiden");
+  formErrorMessage.innerHTML = message;
+  formErrorMessage.scrollIntoView();
+}
+
+function validateForm(event) {
+  if (!loginRegex.test(loginInput.value)) {
+    showErrorMessage("Invalid login. Please follow the specified format.");
+    event.preventDefault();
     return false;
   }
-  if (
-    !phoneField.value.match(
-      /^\s*(\+38)?-?(057)-?(\d{2}|\d{3})-?\d{2}-?\d{2}\s*$/
-    )
-  ) {
-    alert("Уведіть правильний телефон");
-    phoneField.focus();
+  if (!emailRegex.test(emailInput.value)) {
+    showErrorMessage("Invalid email address. Please enter a valid email.");
+    event.preventDefault();
     return false;
   }
+  if (!phoneRegex.test(phoneInput.value)) {
+    showErrorMessage("Invalid phone number. Please enter a 10-digit number.");
+    event.preventDefault();
+    return false;
+  }
+  if (!passwordRegex.test(passwordInput.value)) {
+    showErrorMessage("Invalid password. Please follow the specified format.");
+    event.preventDefault();
+    return false;
+  }
+
   return true;
 }
